125aa853430c14dedaf2d376cd3dcbea97041053
emre kutlu <67983129+3kutlu@users.noreply.github.com>
2022-04-10 06:07:11 +0300

    Update sudokuSolver.py

--- sudokuSolver.py
+++ sudokuSolver.py
@@ -1,50 +1,84 @@
-import numpy as np
+board = [[0, 0, 0, 0, 1, 0, 0, 2, 0],
+         [1, 0, 0, 0, 3, 0, 0, 0, 4],
+         [0, 0, 0, 0, 0, 5, 0, 6, 0],
+         [0, 0, 3, 0, 0, 4, 0, 5, 7],
+         [0, 0, 0, 0, 8, 0, 0, 0, 0],
+         [6, 4, 0, 9, 0, 0, 1, 0, 0],
+         [0, 9, 0, 7, 0, 0, 0, 0, 0],
+         [2, 0, 0, 0, 6, 0, 0, 0, 3],
+         [0, 8, 0, 0, 4, 0, 0, 0, 0]]
 
-grid = [[0, 0, 0, 0, 1, 0, 0, 2, 0],
-        [1, 0, 0, 0, 3, 0, 0, 0, 4],
-        [0, 0, 0, 0, 0, 5, 0, 6, 0],
-        [0, 0, 3, 0, 0, 4, 0, 5, 7],
-        [0, 0, 0, 0, 8, 0, 0, 0, 0],
-        [6, 4, 0, 9, 0, 0, 1, 0, 0],
-        [0, 9, 0, 7, 0, 0, 0, 0, 0],
-        [2, 0, 0, 0, 6, 0, 0, 0, 3],
-        [0, 8, 0, 0, 4, 0, 0, 0, 0]]
 
-solvedSudoku = np.zeros([9, 9], dtype=int)
+def solve(bo):
+    find = find_empty(bo)
+    if not find:
+        return True
+    else:
+        row, col = find
 
-def possible(y, x, n):
-    # checks row
-    for i in range(0, 9):
-        if grid[y][i] == n:
+    for i in range(1, 10):
+        if valid(bo, i, (row, col)):
+            bo[row][col] = i
+
+            if solve(bo):
+                return True
+
+            bo[row][col] = 0
+
+    return False
+
+
+def valid(bo, num, pos):
+    # Check row
+    for i in range(len(bo[0])):
+        if bo[pos[0]][i] == num and pos[1] != i:
             return False
 
-    # checks column
-    for i in range(0, 9):
-        if grid[i][x] == n:
+    # Check column
+    for i in range(len(bo)):
+        if bo[i][pos[1]] == num and pos[0] != i:
             return False
 
-    # checks 3x3 grid within
-    x0 = (x // 3) * 3
-    y0 = (y // 3) * 3
-    for i in range(0, 3):
-        for j in range(0, 3):
-            if grid[y0 + i][x0 + j] == n:
+    # Check box
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+
+    for i in range(box_y * 3, box_y * 3 + 3):
+        for j in range(box_x * 3, box_x * 3 + 3):
+            if bo[i][j] == num and (i, j) != pos:
                 return False
+
     return True
 
-def solve():
-    for y in range(9):
-        for x in range(9):
-            if grid[y][x] == 0:
-                for n in range(1, 10):
-                    if possible(y, x, n):
-                        grid[y][x] = n
-                        solve()
-                        grid[y][x] = 0
-                return
-    for i in range(9):
-        for j in range(9):
-            solvedSudoku[i][j] = grid[i][j]
 
-solve()
-print(solvedSudoku)
+# for visualization
+def print_board(bo):
+    for i in range(len(bo)):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - - - - ")
+
+        for j in range(len(bo[0])):
+            if j % 3 == 0 and j != 0:
+                print(" | ", end="")
+
+            if j == 8:
+                print(bo[i][j])
+            else:
+                print(str(bo[i][j]) + " ", end="")
+
+
+# find empty spots (zeros)
+def find_empty(bo):
+    for i in range(len(bo)):
+        for j in range(len(bo[0])):
+            if bo[i][j] == 0:
+                return i, j  # row, col
+    return None
+
+
+print_board(board)
+solve(board)
+print("--------------------------")
+print("Solved Sudoku : ")
+print("--------------------------")
+print_board(board)
